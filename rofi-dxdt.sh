f082ab90230d3983ab5a6bd78b4eb66a1a99bd11
dannymt97 <dannymt97@gmail.com>
2016-07-26 18:43:27 -0400

    Changed rofi-dxdt.sh link type (soft to hard)

--- rofi-dxdt.sh
+++ rofi-dxdt.sh
@@ -1,1 +1,9 @@
-/home/dtheriault3/Development/dxdt/rofi-dxdt.sh
+#!/usr/bin/env bash
+
+{ 
+    notebook=$( dxdt get | rofi -dmenu -p 'Books:' -no-custom -i ) 
+} && { 
+    page=$( dxdt get $notebook | rofi -dmenu -p 'Page:' -i ) 
+} && {
+    dxdt open $notebook "$page"
+}
